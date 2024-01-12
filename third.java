d905e5e8ff598d584c017378c61db4616d4256f6
netxpertuser <144498226+netxpertuser@users.noreply.github.com>
2024-01-12 23:58:00 +1100

    Update third.java

--- third.java
+++ third.java
@@ -1,3 +1,9 @@
 for (int i = 0; i < 5; i++) {
   System.out.println(i);
 }
+
+int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99f;    // Floating point number
+char myLetter = 'D';         // Character
+boolean myBool = true;       // Boolean
+String myText = "Hello";     // String
